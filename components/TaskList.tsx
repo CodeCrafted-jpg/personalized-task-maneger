8c983db4f2851e157aa09c64ad488bcff4738237
Sayan Mallick <malsayan6@gmail.com>
2026-03-05 00:37:49 +0530

    fixed

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -259,7 +259,7 @@ export default function TaskList() {
 
                 <div className="flex items-center gap-3">
                     <div className="relative">
-                        <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-slate-400" />
+                        {/* <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-slate-400" /> */}
                         <input
                             type="text"
                             placeholder="Search tasks..."
